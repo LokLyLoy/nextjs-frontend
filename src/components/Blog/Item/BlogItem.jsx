4fcb394631211312360d23a9ae847293a2ebca86
LokLyLoy <pavly168@gmail.com>
2025-04-25 21:27:15 +0700

    addBlogSection

--- src/components/Blog/Item/BlogItem.jsx
+++ src/components/Blog/Item/BlogItem.jsx
@@ -1,6 +1,8 @@
+'use client'
 import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
+import * as Icon from '@phosphor-icons/react'
 
 const BlogItem = ({data}) => {
     return (
@@ -17,7 +19,26 @@ const BlogItem = ({data}) => {
 
                 </div>
 
-                <div className='cation2 '></div>
+                <div className='infor sm:p-6 !p-4'>
+                    <div className='caption2 !py-1 !px-3 bg-surface !rounded-full inline-block capitalize bg-blue-100'>
+                        {data.category}
+                    </div>
+
+                    <div className='heading6 !mt-2'>
+                        {data.title}
+                    </div>
+
+                    <div className='date flex items-center !gap-4 !mt-2'>
+                        <div className='author caption2 text-secondary'>
+                            by <span className='text-on-surface'>{data.author}</span>
+                        </div>
+
+                        <div className='item-date flex items-center'>
+                            <Icon.CalendarBlank weight='bold'/>
+                            <span className='!ml-1 caption2'>{data.date}</span>
+                        </div>
+                    </div>
+                </div>
 
             </Link>
 
